a631a8cb0d2094a1f840afb1f165bc12fc2b2673
Danila Osin <danilaosin12@gmail.com>
2020-06-21 02:38:21 +0300

    complete e-mail verification

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const bodyParser = require('body-parser')
 const app = require('express')()
 const jsonParser = bodyParser.json()
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
+
 app.use(jsonParser)
 
 app.use('/api/auth', require('./routes/auth.routes'))
