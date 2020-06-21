2c003a8fb15ab1472cfd235566e2cea38e0ba469
Danila Osin <danilaosin12@gmail.com>
2020-06-21 14:30:25 +0300

    add post model

---
+++ models/Post.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose')
+
+const post = new Schema({
+  text: {
+    type: String,
+    default: '',
+  },
+  img: {
+    type: String,
+    default: '',
+  },
+  author: {
+    type: {
+      username: String,
+      id: String,
+    },
+    required: true,
+  },
+  likes: {
+    type: [
+      {
+        username: String,
+        id: String,
+      },
+    ],
+    default: [],
+  },
+})
+
+module.exports = model('Post', post)
